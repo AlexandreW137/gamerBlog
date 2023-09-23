fe609ea1b487b227630c54f0d8baeaf2c1c3b2b1
AlexandreW137 <87343139+AlexandreW137@users.noreply.github.com>
2023-09-23 01:49:05 -0300

    slavei

--- loginProject/footer.php
+++ loginProject/footer.php
@@ -1,2 +1,10 @@
+<<<<<<< Updated upstream
 </body>
+=======
+
+<link rel="stylesheet" href="https://unpkg.com/bytemd/dist/index.css" />
+    <link rel="stylesheet" href="https://unpkg.com/github-markdown-css" />
+   
+</body>
+>>>>>>> Stashed changes
 </html>
